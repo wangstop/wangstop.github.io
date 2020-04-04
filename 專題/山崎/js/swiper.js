8aea0ce101f3a6eb18898b191cd7a9f111f4c1b8
wangstop <m10612071@gemail.yuntech.edu.tw>
2020-04-04 11:38:10 +0800

    山崎

---
+++ 專題/山崎/js/swiper.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".swiper-container", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    autoplay: {
+        delay: 5000
+    },
+    loop: "true",
+    slidesPerView: "auto",
+    loopedSlides: "3"
+});
+
+var swiper = new Swiper("#swiper2", {
+  autoplay: {
+    delay: 5000
+},
+});
