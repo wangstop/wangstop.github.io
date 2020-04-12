9a9984e7e78b5a000d034bb44b8a46eb5e7d0556
wangstop <61447606+wangstop@users.noreply.github.com>
2020-04-12 15:04:22 +0800

    python

---
+++ python/2.py
@@ -0,0 +1,21 @@
+# x=7
+# print(x) #int
+
+# y=8.1
+# print(y) #float
+
+#numeric
+#int,float,long,bool,complex
+
+#string
+
+#container
+#list ,set ,dict,tuple
+
+# dir()可以查詢當前定義的名稱
+x=1
+y=1
+# id()拿記憶體位址
+# 同樣的內容記憶體位址一樣
+print(id(x))
+print(id(y))
---
+++ python/index.py
@@ -0,0 +1,14 @@
+# print("hello word")
+# py+檔案名稱可以執行檔案內容
+
+# range(11)只0-10
+teninter=[i for i in range(11)]
+# teninter=[1,2,3,4,5,6,7,8,9,10]
+# :={}
+# for in等於for迴圈i=要一個一個取teninter直
+
+for i in teninter:
+    if i%2!=0:
+        print(i)
+        
+print('ok')
