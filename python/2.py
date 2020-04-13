6b0abcc6e07d42af07249e75870d6f6c35f23d4c
wangstop <61447606+wangstop@users.noreply.github.com>
2020-04-13 21:31:35 +0800

    小修改

--- python/2.py
+++ python/2.py
@@ -13,9 +13,36 @@
 #list ,set ,dict,tuple
 
 # dir()可以查詢當前定義的名稱
-x=1
-y=1
+# x=1
+# y=1
 # id()拿記憶體位址
 # 同樣的內容記憶體位址一樣
-print(id(x))
-print(id(y))
+# print(id(x))
+# print(id(y))
+# x=1
+# print('x=',id(x))
+
+# s='hello word'
+# print(s[1])
+
+# l=[0.,1,['a','b','c']]
+# print(l[2][1])
+
+# l[2][1]='alex'
+# print(l[2][1])
+
+# l2=l
+# print(id(l))
+# print(id(2))
+
+# l2[2][1]='alex'
+# print('l=',l[2][1])
+# print('l2=',l2[2][1])
+
+# a=0.1+0.1+0.1
+# print(a,type(a))
+
+k=False
+# '',0,[],{},None=False
+print(k == bool(0))
+
